61e915eca091ecb3dd0f389f2bc9f926b34ca15b
dorian <dorian.hodin@etu.uca.fr>
2024-06-24 12:41:10 +0200

    Adding good front and pagination

--- daidokoro/src/app/component/accueil/accueil.component.ts
+++ daidokoro/src/app/component/accueil/accueil.component.ts
@@ -3,20 +3,27 @@ import {RecipeListComponent} from "../recipe-list/recipe-list.component";
 import {RecipeFormComponent} from "../recipe-form/recipe-form.component";
 import {Recipe} from "../../model/recipe.model";
 import {RecipeService} from "../../service/recipe.service";
+import {NgIf} from "@angular/common";
 @Component({
   selector: 'app-accueil',
   standalone: true,
   imports: [
     RecipeListComponent,
-    RecipeFormComponent
+    RecipeFormComponent,
+    NgIf
   ],
   templateUrl: './accueil.component.html'
 })
 export class AccueilComponent {
+  isFormVisible: boolean = false;
 
   constructor(private recipeService: RecipeService){}
 
   onRecipeSubmitted(recipe : Recipe){
     this.recipeService.addRecipe(recipe);
+  }
+
+  toggleForm() {
+    this.isFormVisible = !this.isFormVisible;
   }
 }
