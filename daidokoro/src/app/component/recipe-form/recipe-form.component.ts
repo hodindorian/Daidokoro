4f43477810362b027e175c2bddbe6cbf84e9b643
dorian <dorian.hodin@etu.uca.fr>
2024-06-26 13:58:01 +0200

    Adding Form Front, and verification of empty field or not

--- daidokoro/src/app/component/recipe-form/recipe-form.component.ts
+++ daidokoro/src/app/component/recipe-form/recipe-form.component.ts
@@ -1,23 +1,23 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {Recipe} from "../../model/recipe.model";
-import {FormControl, FormGroup, ReactiveFormsModule, FormArray, FormBuilder} from "@angular/forms";
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Recipe } from "../../model/recipe.model";
+import { FormControl, FormGroup, ReactiveFormsModule, FormArray, FormBuilder, Validators } from "@angular/forms";
 import { Ingredient } from '../../model/ingredient.model';
 import { Unity } from '../../model/unity';
 import { IngredientService } from '../../service/ingredient.service';
-import {NgFor} from "@angular/common";
+import {NgFor, NgIf} from "@angular/common";
+
 @Component({
   selector: 'app-recipe-form',
   standalone: true,
-  imports: [ReactiveFormsModule,NgFor],
-  templateUrl: './recipe-form.component.html',
-  styleUrl: './recipe-form.component.css'
+  imports: [ReactiveFormsModule, NgFor, NgIf],
+  templateUrl: './recipe-form.component.html'
 })
 export class RecipeFormComponent {
   @Output() formSubmitted = new EventEmitter<Recipe>();
   ingredientsList: Ingredient[] = [];
   unity = Object.values(Unity);
 
-  constructor(private formBuilder: FormBuilder, private ingredientService : IngredientService){}
+  constructor(private formBuilder: FormBuilder, private ingredientService: IngredientService) { }
 
   ngOnInit(): void {
     this.ingredientService.getAll().subscribe(ingredients => {
@@ -26,50 +26,48 @@ export class RecipeFormComponent {
   }
 
   recipeForm: FormGroup = this.formBuilder.group({
-    id: new FormControl('', { nonNullable: true }),
-    name: new FormControl('', { nonNullable: true }),
-    description: new FormControl('', { nonNullable: true }),
+    id: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    name: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    description: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
     ingredients: this.formBuilder.array([])
   });
 
-  get ingredients():FormArray{
+  get ingredients(): FormArray {
     return this.recipeForm.get('ingredients') as FormArray;
   }
 
-  newIngredient(): FormGroup{
+  newIngredient(): FormGroup {
     return this.formBuilder.group({
-      quantity: new FormControl(0, { nonNullable: true }),
-      unit: new FormControl('', { nonNullable: true }),
-      ingredient: new FormControl('', { nonNullable: true })
+      quantity: new FormControl(0, { nonNullable: true, validators: [Validators.required] }),
+      unit: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+      ingredient: new FormControl('', { nonNullable: true, validators: [Validators.required] })
     });
   }
 
   addIngredient() {
     this.ingredients.push(this.newIngredient());
   }
+
   removeIngredient(index: number) {
     this.ingredients.removeAt(index);
   }
 
   onSubmit() {
-    if(this.recipeForm.valid){
+    if (this.recipeForm.valid) {
       const recipe: Recipe = {
         $id: parseInt(this.recipeForm.value.id!),
         $name: this.recipeForm.value.name!,
         $description: this.recipeForm.value.description!,
         $createdAt: new Date(),
-        $ingredients: this.recipeForm.value.ingredients!.map((ingredient : any) => ({
+        $ingredients: this.recipeForm.value.ingredients!.map((ingredient: any) => ({
           quantity: ingredient.quantity,
           unit: ingredient.unit,
           ingredient: this.ingredientsList.find(ing => ing.$name === ingredient.ingredient)
         }))
       };
       this.formSubmitted.emit(recipe);
-      this.recipeForm.reset()
-      this.ingredients.clear()
+      this.recipeForm.reset();
+      this.ingredients.clear();
     }
-
   }
-
-
 }
